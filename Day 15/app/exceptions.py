507b90ca59698b6e9c1c79656fbf433dc8b51273
FINIX52 <flekser333@gmail.com>
2026-06-24 13:52:36 +0300

    Delete Day 15 directory

--- Day 15/app/exceptions.py
+++
@@ -1,8 +0,0 @@
-class DeliveryCalculationError(Exception):
-    """Ошибка при расчёте стоимости доставки"""
-    pass
-
-
-class DeliveryAPIError(Exception):
-    """Ошибка при вызове внешнего API доставки"""
-    pass
